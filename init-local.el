8b321f02c06d42a44d39dab0befdb839fb244abb
zhijia,.zhang <jiahut@gmail.com>
2013-10-10 11:40:24 +0800

    [change] add comment of how to install

--- init-local.el
+++ init-local.el
@@ -1,4 +1,7 @@
-;;Exit insert mode by pressing j and then j quickly
+;; Exit insert mode by pressing j and then j quickly
+;; M-x package-install
+;; key-chord
+
 (require 'key-chord)
 (setq key-chord-two-keys-delay 0.5)
 (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
